ebdd242d6d52d4c1d58b9a498a993160f5628ebf
SawyerWelden <sawyer.welden15@ncf.edu>
2017-03-02 18:44:03 -0500

    made hub. like city but bare

--- src/Hub.java
+++ src/Hub.java
@@ -1,4 +1,13 @@
 
 public class Hub {
-
+	int x,y;
+	String name;
+	public Hub(String name_, int x_, int y_) {
+		name = name_;
+		x = x_;
+		y = y_;
+	}
+	public void print() {
+		System.out.println(name + "(" + x + "," + y + ")");
+	}
 }
